383eb7d0c6d0c33a1160ae3474ff402145ddeb88
clonn <clonncd@gmail.com>
2016-11-04 11:13:27 +0800

    remove no use test case

--- test/unit/controllers/HubbleAPIController.spec.js
+++
@@ -1,9 +0,0 @@
-describe('HubbleAPIController', function() {
-  it('post /api/hubble/:adplaySid', async(done) => {
-    request(sails.hooks.http.app).post('/api/hubble/123').end((err, res) => {
-      let result = res.body;
-      res.statusCode.should.be.equal(200);
-      done();
-    });
-  });
-});
--- test/unit/service/SeoService.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Seo service', () => {
-	it('check is seo', async(done) => {
-		let headers = {'user-agent': 'Facebot'};
-		let result = SeoService.isRobot(headers);
-		result.should.be.true;
-		done();
-	});
-});
